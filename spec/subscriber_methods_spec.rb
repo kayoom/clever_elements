eaefa0054f8e501e2671a1c3fd3f95ae1075135a
Marian Theisen <marian@cice-online.net>
2011-10-21 09:51:02 +0200

    finally figured out api_add_subscriber

--- spec/subscriber_methods_spec.rb
+++ spec/subscriber_methods_spec.rb
@@ -11,7 +11,7 @@ describe CleverElements::Proxy do
   
   describe '#get_subscriber_history' do
     it 'raises error if subscriber does not exist' do
-      savon.expects(:api_get_subscriber_history).returns(:empty)
+      savon.expects(:api_get_subscriber_history).returns(:fault)
       
       proc { subject.get_subscriber_history }.should raise_error Savon::SOAP::Fault
     end
@@ -24,28 +24,82 @@ describe CleverElements::Proxy do
     end
   end
   
-  describe '#api_add_subscriber' do
+  describe '#add_subscriber' do
     it 'raises error if list does not exist' do
       savon.expects(:api_add_subscriber).returns(:fault)
       
       proc { subject.add_subscriber }.should raise_error Savon::SOAP::Fault
     end
     
-    # it 'returns a hash if single result' do
-    #   savon.expects(:api_get_subscriber_details).returns(:success)
-    #   
-    #   response = subject.get_subscriber_details :list_id => '54321'
-    #   response[:item].should be_a Hash
-    #   response[:item][:subscriber_email].should == 'max@muster.net'
-    # end
-    # 
-    # it 'returns an array if multiple results' do
-    #   savon.expects(:api_get_subscriber_details).returns(:multiple)
-    #   
-    #   response = subject.get_subscriber_details :list_id => '54322'
-    #   response[:item].should be_an Array
-    #   response[:item].count.should == 2
-    # end
+    it 'returns a hash on success' do
+      savon.expects(:api_add_subscriber).returns(:success)
+      
+      response = subject.add_subscriber(:subscriber_list => { 
+        :item => {
+          :listID => '54321',
+          :email => 'max@muster.com',
+          :customFields => { :item => []
+          }
+        }
+      })
+      
+      response.should be_a Hash
+    end
+  end
+  
+  describe '#add_subscriber_doi' do
+    it 'raises error if list does not exist' do
+      savon.expects(:api_add_subscriber_doi).returns(:fault)
+      
+      proc { subject.add_subscriber_doi }.should raise_error Savon::SOAP::Fault
+    end
+    
+    it 'returns a Hash on success' do
+      savon.expects(:api_add_subscriber_doi).returns(:success)
+      
+      response = subject.add_subscriber_doi(:subscriber_list => { 
+        :item => {
+          :listID => '54321',
+          :email => 'max@muster.com',
+          :customFields => { :item => [] }
+        }
+      })
+      
+      response.should be_a Hash
+    end
+  end
+  
+  describe '#delete_subscriber' do
+    it 'raises error if subscriber id is invalid' do
+      savon.expects(:api_delete_subscriber).returns(:fault)
+      
+      proc { subject.delete_subscriber }.should raise_error Savon::SOAP::Fault
+    end
+    
+    it 'returns 200 on success' do
+      savon.expects(:api_delete_subscriber).returns(:success)
+      
+      response = subject.delete_subscriber :subscriber_id_list_short => { :item => { :subscriber_id => '123456789'}}
+      response.should == '200'
+    end
+  end
+  
+  describe '#unsubscribe_subscriber_from_list' do
+    it 'raises error if list id is invalid' do
+      savon.expects(:api_unsubscribe_subscriber_from_list).returns(:fault)
+      
+      proc { subject.unsubscribe_subscriber_from_list }.should raise_error Savon::SOAP::Fault
+    end
+    
+    it 'returns 200 on success' do
+      savon.expects(:api_unsubscribe_subscriber_from_list).returns(:success)
+      
+      response = subject.unsubscribe_subscriber_from_list :subscriber_id_list => { :item => { :subscriber_id => '123456789', :list_id => 54321}}
+      response.should == '200'
+    end
+  end
+  
+  describe '#unsubscribe_subscriber_from_all' do
   end
   
   # describe '#get_subscriber_unsubscribes' do
